9cc77398885191802068389d7afed2d1c9b9b215
TZQ <1661610563@qq.com>
2020-07-16 16:00:53 +0800

    2020/7/16 16:00

--- PlatinumManual/main.c
+++ PlatinumManual/main.c
@@ -8,51 +8,122 @@
 #include "hyk.h"
 
 
+long long int id = -1; // 当前使用程序的用户的id
 
-void welcome();
-int get_choice(int amount);
-
-long long int id;
+void welcome(); // 欢迎界面
+int get_choice(int amount); // 获取choice选择
+void main_menu(); // 主界面
 
 
 int main() {
-	welcome();
 
+	/*
+	 * 若注册失败或登陆失败，两个函数均会将-1赋值给main.c的全局变量id；
+	 * 若注册成功或登陆成功，两个函数均会将当前用户的id赋值给main.c的全局变量id。
+	 */
+	while (id == -1) {
+		welcome();
+	}
 
+	/*
+	 * 在此设置死循环，
+	 * 当用户从相应的功能模块退出后，将会继续弹出主界面，
+	 * 让用户继续选择想要执行的操作。
+	 * 当用户选择“退出”操作时，程序将调用exit(1)函数，退出程序（也退出循环）。
+	 */
+	while (1) {
+		main_menu();
+	}
 
-	return 0;
 }
 
 void welcome() {
-	int sign_up();
-	int log_in();
-
 	printf("\n");
-	printf("--------------------------------------------------------------------------------------------\n");
-	printf("|★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★|\n");
-	printf("|★★★                                欢迎使用学生学籍管理系统                                ★★★|\n");
-	printf("|★★★                                     请选择注册或者登陆                                      ★★★|\n");
-	printf("|★★★                                       1. 注册      2. 登陆                                        ★★★|\n");
-	printf("|★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★|\n");
-	printf("--------------------------------------------------------------------------------------------\n");
+	printf("★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★\n");
+	printf("                         欢迎使用白金手册！！！\n");
+	printf("                           请选择注册或者登陆\n");
+	printf("                             1. 注册      2. 登陆\n");
+	printf("★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★\n");
 	printf("\n");
 
 	int num_of_choices = 2;
-	int choice = get_choice(num_of_choices);
+	int choice = get_choice(num_of_choices); // 获得用户所要进行的操作对应的选项
 
-	if (choice == 1) {
-		id = sign_up();
-	} else if (choice == 2) {
-		id = log_in();
+	// 根据choice进行选择
+	switch (choice) {
+		case 1:
+			id = sign_up();
+			break;
+		case 2:
+			id = log_in();
+			break;
+		default:
+			break;
 	}
 }
 
 int get_choice(int amount) {
 	int choice;
+	printf("请输入：");
 	scanf("%d", &choice);
+
+	// 确保用户输入的数在1~amount之间
 	while (!(0 < choice && choice <= amount)) {
 		printf("输出错误！\n请重新输出一个从1至%d的数。\n", amount);
 		scanf("%d", &choice);
 	}
 	return choice;
+}
+
+void main_menu() {
+	printf("\n");
+	printf("★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★\n");
+	printf("                       欢迎进入白金手册主界面！！！\n");
+	printf("                            请选择您要进行的操作：\n");
+	printf("                                 1. 网页夹管理\n");
+	printf("                                 2. 通讯录管理\n");
+	printf("                                 3. 日记管理\n");
+	printf("                                 4. 密码管理\n");
+	printf("                                 5. 日程管理\n");
+	printf("                                 6. 个人密码修改\n");
+	printf("                                 7. 个人信息查看\n");
+	printf("                                 8. 删除用户数据\n");
+	printf("                                 9. 退出程序\n");
+	printf("★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★\n");
+	printf("\n");
+
+	int num_of_choices = 9;
+	int choice = get_choice(num_of_choices); // 获得用户所要进行的操作对应的选项
+
+	// 根据choice进行选择
+	switch (choice) {
+		case 1:
+			manage_url_of_web_page(id);
+			break;
+		case 2:
+			manage_address_list(id);
+			break;
+		case 3:
+			manage_diary(id);
+			break;
+		case 4:
+			manage_password(id);
+			break;
+		case 5:
+			manage_schedule(id);
+			break;
+		case 6:
+			update_password(id);
+			break;
+		case 7:
+			show_information(id);
+			break;
+		case 8:
+			delete_information(id);
+			break;
+		case 9:
+			exit(1);
+		default:
+			break;
+	}
 }
